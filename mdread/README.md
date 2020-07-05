b8c5c0504eb9830fedd0146ccc64243bce9b61b8
Sagar Sahu <sagarsahu27@hotmail.com>
2020-07-06 01:03:02 +0530

    Update App 1 with HTML wrap

---
+++ mdread/README.md
@@ -0,0 +1,11 @@
+How to Run 
+==========
+
+This needs nightly version of to run
+*rustup install nightly*
+
+and to compile this and run, use this command:
+*cargo +nightly run -- test_data/hello.md*
+
+OR make nightly as default rust
+*rustup override set nightly*
--- mdread/src/main.rs
+++ mdread/src/main.rs
@@ -1,5 +1,27 @@
+#![feature(proc_macro_hygiene)]
+
 use clap::{clap_app, crate_version};
+use maud::html;
 use pulldown_cmark::{html::push_html, Event, Parser};
+
+fn wrap_html(s:&str, css:Option<&str>)->String{
+    let res = html!{
+        (maud::DOCTYPE)
+        html {
+            head {
+                meta charset = "utf-8";
+                @if let Some(s) = css {
+                    link rel="stylesheet" type="text/css" href=(s) {}
+                }
+
+            }
+            body {
+                (maud::PreEscaped(s))
+            }
+        }
+    };
+    res.into_string()
+}
 
 fn main() {
     let clap = clap_app!(mdrend => 
@@ -7,17 +29,26 @@ fn main() {
                             (author:"Sagar Sahu")
                             (about:"Renders markdown as you like")
                             (@arg input: +required "Sets the input file")
+                            (@arg wrap: -w "Wrap in HTML")
+                            (@arg event: -e "Link Event")
+                            (@arg css: --css +takes_value "Link to css")
     ).get_matches();
 
     let infile = std::fs::read_to_string(clap.value_of("input").unwrap()).expect("Could not read file");
 
     let ps = Parser::new(&infile);
-    let mut res = String::new();
-
     let ps : Vec<Event> = ps.into_iter().collect();
-    for p in &ps {
-        println!("{:?}", p);
+    if clap.is_present("event") {
+        for p in &ps {
+            println!("{:?}", p);
+        }
     }
+
+    let mut res = String::new();
     push_html(&mut res, ps.into_iter());
+
+    if clap.is_present("wrap") {
+        res = wrap_html(&res, clap.value_of("css"));
+    }
     println!("{}", res);
 }
